f4e33fa95991788708f74f4f6fb511e85d909c39
Karine Vieira <kv.vieira1@gmail.com>
2022-07-15 08:56:33 -0300

    🔧 add UUID as primary key

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.config.generators do |g|
+  g.orm :active_record, primary_key_type: :uuid
   g.factory_bot suffix: "factories"
   g.fixture_replacement :factory_bot, dir: "spec/factories"
   g.test_framework :rspec
