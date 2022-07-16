10017007c0e7ced724694966b7e030e451b12331
Karine Vieira <kv.vieira1@gmail.com>
2022-07-16 10:14:37 -0300

    ✨ create Structure::CardComponent

---
+++ app/components/structure/card_component.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Structure
+  class CardComponent < ViewComponent::Base
+    renders_one :subtitle, "SubtitleComponent"
+
+    def all_classes
+      "max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+    end
+
+    class SubtitleComponent < ViewComponent::Base
+      def call
+        classes = "mb-3 text-sm font-normal text-gray-700 dark:text-gray-400"
+        content_tag :p, content, class: classes
+      end
+    end
+  end
+end
