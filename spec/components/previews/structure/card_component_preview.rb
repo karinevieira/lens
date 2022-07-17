7265e442db75c0c054b46b3869a0bef3c4ca06d3
Karine Vieira <62672837+karinevieira@users.noreply.github.com>
2022-07-17 09:47:19 -0300

    ✨ add ImageComponent as CardComponent slot (#3)

--- spec/components/previews/structure/card_component_preview.rb
+++ spec/components/previews/structure/card_component_preview.rb
@@ -2,9 +2,11 @@
 
 module Structure
   class CardComponentPreview < ViewComponent::Preview
+    # @param image
     # @param subtitle
-    def playground(subtitle: "Photo subtitle")
+    def playground(subtitle: "Photo subtitle", image: "https://flowbite.com/docs/images/blog/image-1.jpg")
       render Structure::CardComponent.new do |card|
+        card.image { image }
         card.subtitle { subtitle }
       end
     end
