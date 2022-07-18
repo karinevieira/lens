d9b71b59f9600b46224d1e36d9cc7d02965ae615
Karine Vieira <kv.vieira1@gmail.com>
2022-07-18 20:21:03 -0300

    ✨ create Form::Label

---
+++ spec/components/form/label_component_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Form::LabelComponent, type: :component do
+  context "when content is present" do
+    subject(:rendered) { render_inline(described_class.new) { "Label" } }
+
+    it "renders without problems" do
+      expect(rendered.to_html).to be_present
+    end
+  end
+
+  context "when content is empty" do
+    subject(:rendered) { render_inline(described_class.new) }
+
+    it "doesn't render" do
+      expect(rendered.to_html).to be_empty
+    end
+  end
+end
