d55d01424e3a73b529800a3dc95dc48ace3f8128
Karine Vieira <kv.vieira1@gmail.com>
2022-08-07 09:43:40 -0300

    ✨ add create post action

--- app/pages/posts/new_page.rb
+++ app/pages/posts/new_page.rb
@@ -2,5 +2,10 @@
 
 module Posts
   class NewPage < ApplicationPage
+    attr_reader :post
+
+    def initialize(post:)
+      @post = post
+    end
   end
 end
---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Posts", type: :request do
+  describe "GET /new" do
+    it "returns http status ok" do
+      get new_post_path
+
+      expect(response).to have_http_status :ok
+    end
+  end
+
+  describe "POST /posts" do
+    it "returns http status found" do
+      params = { post: { subtitle: "My test subtitle" } }
+      post posts_path, params: params
+
+      expect(response).to have_http_status :found
+    end
+  end
+end
