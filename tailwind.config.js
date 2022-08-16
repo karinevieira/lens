f0926d314a437e1fb754c2cf74dcb2ba4dd8b77b
Karine Vieira <62672837+karinevieira@users.noreply.github.com>
2022-08-16 18:06:35 -0300

    🐞 add tailwind config for classes to work on pages (#21)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/components/**/*.html.erb',
     './app/components/**/*.rb',
+    './app/pages/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
