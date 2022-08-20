6d595d9d78b1ba9c2d9f8c33bdf3d39f68081b63
Karine Vieira <62672837+karinevieira@users.noreply.github.com>
2022-08-20 11:40:11 -0300

    ✨ add edit button to CardComponent (#27)

--- app/components/action/button_component.rb
+++ app/components/action/button_component.rb
@@ -8,7 +8,7 @@ module Action
       blue: "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300
       font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600
       dark:hover:bg-blue-700 dark:focus:ring-blue-800".squish,
-      white: "text-gray-900 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800
+      white: "text-gray-900 font-medium rounded-lg text-sm dark:bg-gray-800
       dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600
       dark:focus:ring-gray-700".squish
     }.freeze
