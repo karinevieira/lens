285a3df596634f07594c82cb8ba5e8ae38e8cc25
Karine Vieira <62672837+karinevieira@users.noreply.github.com>
2022-08-20 11:14:52 -0300

    ✨ add white color option for ButtonComponent (#26)

--- spec/components/previews/action/button_component_preview.rb
+++ spec/components/previews/action/button_component_preview.rb
@@ -13,5 +13,17 @@ module Action
       render(Action::ButtonComponent.new(tag: :link, href: "http://localhost:3000")) { "Go to home" }
     end
     # @!endgroup
+
+    # @!group Colors
+    # @label Blue
+    def with_blue_color
+      render(Action::ButtonComponent.new) { "Create post" }
+    end
+
+    # @label White
+    def with_white_color
+      render(Action::ButtonComponent.new(color: :white, tag: :link, href: "http://localhost:3000")) { "Edit post" }
+    end
+    # @!endgroup
   end
 end
