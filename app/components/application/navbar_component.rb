7599f018daf4ee60a95dc7a57a3b715720c7e756
Karine Vieira <62672837+karinevieira@users.noreply.github.com>
2022-12-12 18:43:54 -0300

    ✨ create Application::Navbar (#38)

---
+++ app/components/application/navbar_component.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Application
+  class NavbarComponent < ViewComponent::Base
+  end
+end
--- spec/components/previews/structure/navbar_component_preview.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Structure
-  class NavbarComponentPreview < ViewComponent::Preview
-    def default; end
-  end
-end
