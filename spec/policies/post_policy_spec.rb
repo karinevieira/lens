0fe6595a6c3a68430b2b0a4a6815537cf692feb0
Karine Vieira <kv.vieira1@gmail.com>
2024-05-05 09:41:56 -0300

    ♻️ add alias rules to PostPolicy

--- spec/policies/post_policy_spec.rb
+++ spec/policies/post_policy_spec.rb
@@ -4,33 +4,33 @@ require "rails_helper"
 
 RSpec.describe PostPolicy do
   describe "#index?" do
-    it "allows everyone to see the posts" do
+    it "is an alias of :create? authorization rule" do
       current_user = build_stubbed(:user)
-      post = build_stubbed(:post, user: current_user)
+      post = build_stubbed(:post)
+
       policy = described_class.new(post, user: current_user)
+      rule = :index?
 
-      result = policy.apply(:index?)
-
-      expect(result).to be true
+      expect(rule).to be_an_alias_of(policy, :create?)
     end
   end
 
   describe "#new?" do
-    it "allows everyone to access the page to create a post" do
+    it "is an alias of :create? authorization rule" do
       current_user = build_stubbed(:user)
-      post = build_stubbed(:post, user: current_user)
+      post = build_stubbed(:post)
+
       policy = described_class.new(post, user: current_user)
+      rule = :new?
 
-      result = policy.apply(:new?)
-
-      expect(result).to be true
+      expect(rule).to be_an_alias_of(policy, :create?)
     end
   end
 
   describe "#create?" do
     it "allows everyone to create a post" do
       current_user = build_stubbed(:user)
-      post = build_stubbed(:post, user: current_user)
+      post = build_stubbed(:post)
       policy = described_class.new(post, user: current_user)
 
       result = policy.apply(:create?)
@@ -40,29 +40,14 @@ RSpec.describe PostPolicy do
   end
 
   describe "#edit?" do
-    context "when current user is the post creator" do
-      it "allows to access the page to edit the post" do
-        current_user = build_stubbed(:user)
-        post = build_stubbed(:post, user: current_user)
-        policy = described_class.new(post, user: current_user)
+    it "is an alias of :update? authorization rule" do
+      current_user = build_stubbed(:user)
+      post = build_stubbed(:post, user: current_user)
 
-        result = policy.apply(:edit?)
+      policy = described_class.new(post, user: current_user)
+      rule = :edit?
 
-        expect(result).to be true
-      end
-    end
-
-    context "when current user isn't the post creator" do
-      it "not allow to access the page to edit the post" do
-        current_user = build_stubbed(:user)
-        creator = build_stubbed(:user)
-        post = build_stubbed(:post, user: creator)
-        policy = described_class.new(post, user: current_user)
-
-        result = policy.apply(:edit?)
-
-        expect(result).to be false
-      end
+      expect(rule).to be_an_alias_of(policy, :update?)
     end
   end
 
@@ -94,29 +79,14 @@ RSpec.describe PostPolicy do
   end
 
   describe "#destroy?" do
-    context "when current user is the post creator" do
-      it "allows to destroy the post" do
-        current_user = build_stubbed(:user)
-        post = build_stubbed(:post, user: current_user)
-        policy = described_class.new(post, user: current_user)
+    it "is an alias of :update? authorization rule" do
+      current_user = build_stubbed(:user)
+      post = build_stubbed(:post, user: current_user)
 
-        result = policy.apply(:destroy?)
+      policy = described_class.new(post, user: current_user)
+      rule = :destroy?
 
-        expect(result).to be true
-      end
-    end
-
-    context "when current user isn't the post creator" do
-      it "not allow to delete the post" do
-        current_user = build_stubbed(:user)
-        creator = build_stubbed(:user)
-        post = build_stubbed(:post, user: creator)
-        policy = described_class.new(post, user: current_user)
-
-        result = policy.apply(:destroy?)
-
-        expect(result).to be false
-      end
+      expect(rule).to be_an_alias_of(policy, :update?)
     end
   end
 end
