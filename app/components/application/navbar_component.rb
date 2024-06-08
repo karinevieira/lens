a97c19f804719517170a29675597ff10c8bd4476
Karine Vieira <62672837+karinevieira@users.noreply.github.com>
2024-06-08 09:37:47 -0300

    ✨ activates navbar items (#90)

--- app/components/application/navbar_component.rb
+++ app/components/application/navbar_component.rb
@@ -2,12 +2,29 @@
 
 module Application
   class NavbarComponent < ViewComponent::Base
-    def initialize(current_user:)
+    def initialize(current_user:, current_controller:)
       @current_user = current_user
+      @current_controller = current_controller
     end
 
     private
 
-    attr_reader :current_user
+    attr_reader :current_user, :current_controller
+
+    def activate_home_item?
+      current_controller.is_a?(PostsController) && current_action_is_a?(:index)
+    end
+
+    def activate_new_post_item?
+      current_controller.is_a?(PostsController) && current_action_is_a?(:new)
+    end
+
+    def activate_profile_item?
+      current_controller.is_a?(UsersController) || current_controller.is_a?(ProfilesController)
+    end
+
+    def current_action_is_a?(action)
+      current_controller.action_name == action.to_s
+    end
   end
 end
