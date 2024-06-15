fa19852fb11cbe2c4b1019972e9f197302589a72
Karine Vieira <kv.vieira1@gmail.com>
2024-06-15 14:04:34 -0300

    ♻️ separate card component into other components

--- app/components/posts/card_component.rb
+++ app/components/posts/card_component.rb
@@ -2,10 +2,6 @@
 
 module Posts
   class CardComponent < ViewComponent::Base
-    include ActionPolicy::Behaviour
-
-    attr_reader :post, :user
-
     with_collection_parameter :post
 
     def initialize(post:, user:)
@@ -15,38 +11,10 @@ module Posts
 
     private
 
+    attr_reader :post, :user
+
     def image
-      image_tag(url_for(post.image)) if post.image.attached?
-    end
-
-    def edit_action
-      render(Action::LinkComponent.new(href: edit_post_path(post))) { t("posts.card_component.edit") }
-    end
-
-    def delete_action
-      render(
-        Action::LinkComponent.new(
-          color: :red,
-          href: post_path(post),
-          data: { turbo_method: :delete, turbo_frame: "_top" }
-        )
-      ) { t("posts.card_component.delete") }
-    end
-
-    def show_action?(rule:)
-      allowed_to?(rule, post, context: { user: user })
-    end
-
-    def already_liked?
-      ::Like.exists?(user_id: user.id, post_id: post.id)
-    end
-
-    def like
-      ::Likes::Find.result(user_id: user.id, post_id: post.id).like
-    end
-
-    def likes_count_text
-      t("posts.card_component.likes_count", count: post.likes.count)
+      image_tag(url_for(post.image))
     end
   end
 end
