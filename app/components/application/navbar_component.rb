3958add975dc84f411798204f7313729c5bee7a0
Karine Vieira <62672837+karinevieira@users.noreply.github.com>
2024-06-23 12:10:30 -0300

    ♻️ use current_page? helper in the Application::NavbarComponent (#98)

--- app/components/application/navbar_component.rb
+++ app/components/application/navbar_component.rb
@@ -2,7 +2,7 @@
 
 module Application
   class NavbarComponent < ViewComponent::Base
-    def initialize(current_user:, current_controller:)
+    def initialize(current_user:, current_controller: nil)
       @current_user = current_user
       @current_controller = current_controller
     end
@@ -12,19 +12,15 @@ module Application
     attr_reader :current_user, :current_controller
 
     def activate_home_item?
-      current_controller.is_a?(PostsController) && current_action_is_a?(:index)
+      current_page?(controller: "posts", action: "index")
     end
 
     def activate_new_post_item?
-      current_controller.is_a?(PostsController) && current_action_is_a?(:new)
+      current_page?(controller: "posts", action: "new")
     end
 
     def activate_profile_item?
       current_controller.is_a?(UsersController) || current_controller.is_a?(ProfilesController)
-    end
-
-    def current_action_is_a?(action)
-      current_controller.action_name == action.to_s
     end
   end
 end
--- spec/components/application/navbar_component_spec.rb
+++ spec/components/application/navbar_component_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Application::NavbarComponent, type: :component do
   it "renders the Home item" do
     current_user = create(:user)
 
-    rendered = render_inline(described_class.new(current_user: current_user, current_controller: "posts"))
+    rendered = render_inline(described_class.new(current_user: current_user))
     item_text = I18n.t("application.navbar_component.home")
 
     expect(rendered.to_html).to have_link(item_text, href: root_path)
@@ -15,7 +15,7 @@ RSpec.describe Application::NavbarComponent, type: :component do
   it "renders the Search item" do
     current_user = create(:user)
 
-    rendered = render_inline(described_class.new(current_user: current_user, current_controller: "posts"))
+    rendered = render_inline(described_class.new(current_user: current_user))
     item_text = I18n.t("application.navbar_component.search")
 
     expect(rendered.to_html).to have_link(item_text, href: "#")
@@ -24,7 +24,7 @@ RSpec.describe Application::NavbarComponent, type: :component do
   it "renders the New item" do
     current_user = create(:user)
 
-    rendered = render_inline(described_class.new(current_user: current_user, current_controller: "posts"))
+    rendered = render_inline(described_class.new(current_user: current_user))
     item_text = I18n.t("application.navbar_component.new")
 
     expect(rendered.to_html).to have_link(item_text, href: new_post_path)
@@ -33,7 +33,7 @@ RSpec.describe Application::NavbarComponent, type: :component do
   it "renders the Profile item" do
     current_user = create(:user)
 
-    rendered = render_inline(described_class.new(current_user: current_user, current_controller: "posts"))
+    rendered = render_inline(described_class.new(current_user: current_user))
     item_text = I18n.t("application.navbar_component.profile")
 
     expect(rendered.to_html).to have_link(item_text, href: user_path(username: current_user.profile.username))
@@ -42,7 +42,7 @@ RSpec.describe Application::NavbarComponent, type: :component do
   it "renders the Sign out item" do
     current_user = create(:user)
 
-    rendered = render_inline(described_class.new(current_user: current_user, current_controller: "posts"))
+    rendered = render_inline(described_class.new(current_user: current_user))
     item_text = I18n.t("application.navbar_component.sign_out")
 
     expect(rendered.to_html).to have_link(item_text, href: destroy_user_session_path)
